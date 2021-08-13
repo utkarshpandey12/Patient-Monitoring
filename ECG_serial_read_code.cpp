5b5579451bf6fd7290b4d17c05d87f8f220a0ce3
utkarshpandey12 <u13051995p@gmail.com>
2021-08-13 15:21:56 +0530

    update ecg.cpp

--- ECG_serial_read_code.cpp
+++ ECG_serial_read_code.cpp
@@ -3,6 +3,7 @@
 #include <bits/stdc++.h>
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -13,17 +14,70 @@ string convertToString(char* a, int size)
     return s;
 }
 
+double StringToFloat(char str[100])
+{
+    double result = 0.0;
+    int index = 0;
+    int temp = 0;
+    int flag = 1;
+    while (str[index] != '\0')
+    {
+        if (str[index] != '.')
+        {
+            temp = int(str[index])- 48;
+            if (temp >= 0 && temp <= 9)
+                {
+                    result = temp+ result*10 ;
+                    index++;
+                }
+                else
+                {
+                    flag = 0;
+                    break;
+                }
+        // temp = 0;
+        }
+        else if(str[index] == '.')
+        {
+            float devider = 10;
+            index += 1;
+            while (str[index] != '\0')
+             {
+                temp = int(str[index])- 48;
+                if (temp >= 0 && temp <= 9)
+                    {
+                        temp = int(str[index])- 48;
+                        if (temp >= 0 && temp <= 9)
+                        {
+                            result = (temp/devider)+ result;
+                            index++;
+                            devider = devider * 10;
+                        }
+                        else
+                        {
+                            flag = 0;
+                            break;
+                        }
+                    }
+             }
+        }
+    }
+    return result;
+}
+
 int main()
 { 
- //ofstream myfile; 
- int serial_port;
- char input;
- //char input1;
- serial_port = serialOpen("/dev/ttyUSB0",115200);  
- //char a[100] = {'4'};
-int cnt =0;
-int index = 0;
-int flag = 0;
+  //ofstream myfile; 
+  int serial_port;
+  char input;
+  //char input1;
+  serial_port = serialOpen("/dev/ttyUSB0",115200);  
+  //char a[100] = {'4'};
+ int cnt =0;
+ int index = 0;
+ int flag = 0;
+ int count_avg = 0;
+
  //string array_string;
  //int buffer_size = 0;
  //float val1;
@@ -32,14 +86,23 @@ int flag = 0;
  char array[25];
  char temp1[15];
  char temp2[15];
+ double f1, f2;
+ double ecg_avg , rr_avg;
  //string s_a;
 
  while (1) 
  {
      if (serialDataAvail(serial_port))
-     {  
+     { 
        while (serialDataAvail(serial_port))
-       {
+       { 
+         if (count_avg>9)
+         {
+         count_avg = 0;
+         ecg_avg = 0;
+         rr_avg = 0;
+         } 
+         
          array[0] = '\0';
          input = serialGetchar(serial_port);
          while(input !='\n'){
@@ -55,8 +118,12 @@ int flag = 0;
           //  cout<<input;
          }
         array[n]='\0';
+        //cout<<array<<endl;
         index = 0;
         cnt = 0;
+        flag = 0;
+        temp1[0] = '\0';
+        temp2[0] = '\0';
         //int buffer_size = sizeof(array) / sizeof(char);
         while (array[index]!='\0')
         { 
@@ -65,7 +132,7 @@ int flag = 0;
           index++;
           cnt++;
           }
-          if (flag==1){
+          if (flag==1 && array[index]!=','){
           temp2[cnt] = array[index];
           index++;
           cnt++;
@@ -75,15 +142,24 @@ int flag = 0;
             cnt = 0;
             flag = 1;
             temp1[index] = '\0';
+            index++;
 
           }
 
 
         }
         temp2[cnt] = '\0';
-        
+        //cout<<temp1<<" "<<temp2<<endl;
+        f1 = atof (temp1);
+        f2 = atof (temp2);
+        //cout<<f1<<" "<<f2<<endl;
+        ecg_avg +=f1;
+        rr_avg += f2;
+        count_avg++;
 
-        cout<<temp1<<temp2<<endl;
+
+
+        //cout<<temp1<<temp2<<endl;
         //int buffer_size = sizeof(array) / sizeof(char);
         //s_a = convertToString(array, buffer_size);
         //cout<<s_a<<endl;
@@ -111,10 +187,12 @@ int flag = 0;
         //   cout<<input<<endl;
         // }
 
-        }
-        
       }
- 
-   }
+
+    }
+      
+  }
 
 }
+
+
